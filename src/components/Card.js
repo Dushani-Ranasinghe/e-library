c21a69a553fd283a20a6112724ff3d4d14c62bcf
Dushani-Ranasinghe <uththaradushani@gmail.com>
2024-08-08 00:11:25 +0530

    connected google api to books card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,39 @@
 import React from "react";
 import "./card.css";
-import Book1 from "../assets/books/got.jpg";
 
-const Card = () => {
+const Card = ({ book }) => {
+  console.log(book);
   return (
-    <div className="card">
-      <img src={Book1} alt="got" className="book-img" />
-      <div className="bottom">
-        <h3 className="title">Game of Thrones</h3>
-        <p className="amount">&#8377;3290</p>
-      </div>
-    </div>
+    <>
+      {book.map((item) => {
+        let thumbnail =
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.smallThumbnail;
+        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+        let title = item.volumeInfo.title; // Extract the title
+        let saleability = item.saleInfo.saleability; // Extract the saleability
+
+        if (thumbnail != undefined) {
+          return (
+            <div className="card" key={item.id}>
+              {" "}
+              {/* Added key for each item */}
+              <img src={thumbnail} alt={title} className="book-img" />
+              <div className="bottom">
+                <h3 className="title">{title}</h3> {/* Render the title */}
+                {saleability === "NOT_FOR_SALE" ? (
+                  <p className="amount">Not for sale</p>
+                ) : saleability === "FREE" ? (
+                  <p className="amount">Free</p>
+                ) : (
+                  <p className="amount">&#8377;{amount}</p>
+                )}
+              </div>
+            </div>
+          );
+        }
+      })}
+    </>
   );
 };
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.css";
 import Card from "./Card";
+import axios from "axios";
 
 export const Main = () => {
+  const [search, setSearch] = useState("");
+  const [bookData,setData]= useState([]);
+  const searchbook= (e)=>{
+    if(e.key === "Enter"){
+        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyDkvobdhpM8i-USZyZIBiXlhCc9N-VCYQI'+'&maxResults=40')
+        .then(res=>setData(res.data.items))
+        .catch(err=>console.log(err))
+    }
+  }
+
   return (
     <div className="main-container">
       <div className="header">
@@ -10,13 +21,13 @@ export const Main = () => {
           <h1>Explore the World of Books</h1>
           <p>
             Discover a vast collection of books at your fingertips with our
-            e-library <br/> powered by the Google Books API
+            e-library <br /> powered by the Google Books API
           </p>
         </div>
         <div className="row2">
           <h3>Find your book</h3>
           <div className="search">
-            <input type="text" placeholder="Enter Book Name" />
+            <input type="text" placeholder="Enter Book Name" value={search} onChange={e=>setSearch(e.target.value)} onKeyDown={searchbook}/>
             <button>
               {" "}
               <i className="fas fa-search"></i>
@@ -25,12 +36,10 @@ export const Main = () => {
         </div>
       </div>
       <div className="container">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        
+        {
+            <Card book={bookData}/>
+        }
       </div>
     </div>
   );
